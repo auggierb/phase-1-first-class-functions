dbd174ccf9d6dc2103cdbba71b19aa54fd37ce11
August <auggierutter@gmail.com>
2021-05-04 21:16:27 -0400

    commit tree bb411d042173e8e856039bbd4e579c2f9ad9dd3e

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback)  {
+callback()
+}
+function returnsANamedFunction() {
+return function iAmAFunction(){
+    
+}}
+function returnsAnAnonymousFunction(){
+    return function (){
+        
+    }
+}
